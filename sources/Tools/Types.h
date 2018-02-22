f53dfeec2299be6b692c58a75288501420896c3f
CDLB <CDLB@CDLB-PC>
2018-02-22 01:41:35 +0100

    Add integers platforms dependent

--- sources/Tools/Types.h
+++ sources/Tools/Types.h
@@ -3,3 +3,11 @@
 #include <assert.h>
 
 #define ASSERT assert
+
+#ifdef WIN32
+typedef unsigned int			ux;
+typedef int						ix;
+#else
+typedef unsigned long long int	ux;
+typedef long long int			ix;
+#endif

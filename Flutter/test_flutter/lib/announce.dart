06cf31446070693ee8de7178f6dc783a7916a21f
Tuf <christophe.delabrouhe@wildsheepstudio.com>
2020-11-04 16:43:40 +0100

    Merge

--- Flutter/test_flutter/lib/announce.dart
+++ Flutter/test_flutter/lib/announce.dart
@@ -1,56 +1,71 @@
 import 'package:flutter/material.dart';
 
-class Announce {
-  static String m_title = 'Paris 18ème - Appartement 3 pièces';
-
-  static int m_price = 437000;
-
-  static double m_rate = 10.7;
-
-  static String m_description =
+class AnnounceData {
+  String title = 'Paris 18ème - Appartement 3 pièces';
+  int price = 437000;
+  double rate = 10.7;
+  String description =
       'Paris 18ème - Abbesses - Dans une charmante copropriété Montmartroise calme et verdoyante, idéalement située proche de la rue des Abbesses et de la rue Lepic, un appartement composé d une grande pièce principale, avec une cuisine aménagée et un coin bureau, une salle d eau et un w.-c. séparé. Au sous-sol, une cave. Honoraires d agence à la charge de l acquéreur inclus :  4,61 % soit 20 000 € d honoraires. Prix hors honoraires : 434 000 €.';
-  static String m_URL =
+  String announceURL =
       'https://www.laforet.com/agence-immobiliere/paris18abbesses/acheter/paris-18/appartement-1-piece-19493910';
-  static String m_imageURL =
-      'https://laforetbusiness.laforet-intranet.com/office9/laforet_paris11bastille/catalog/images/pr_p/1/9/2/9/1/9/3/0/19291930a.jpg';
+  String imageURL =
+      'https://laforetbusiness.laforet-intranet.com/office9/laforet_gaillon/catalog/images/pr_p/1/9/5/0/9/9/3/2/19509932a.jpg';
 
-  static Widget titleSection = Container(
-    padding: const EdgeInsets.all(16),
-    child: Row(
-      children: [
-        Expanded(
-          /*1*/
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              /*2*/
-              Container(
-                padding: const EdgeInsets.only(bottom: 8),
-                child: Text(
-                  m_title,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
+  AnnounceData(
+      {this.title,
+      this.price,
+      this.rate,
+      this.description,
+      this.announceURL,
+      this.imageURL});
+}
+
+class Announce {
+  AnnounceData data;
+
+  Announce(AnnounceData _data) {
+    data = _data;
+  }
+
+  Widget CreateTitleSection() {
+    return new Container(
+      padding: const EdgeInsets.all(16),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                /*2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    '${data.title}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-              Text(
-                m_price.toString(),
-                style: TextStyle(
-                  color: Colors.grey[500],
+                Text(
+                  'Text: ${data.price.toString()}',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-        /*3*/
-        Icon(
-          Icons.star_border,
-          color: Colors.red[500],
-        ),
-        Text('41'),
-      ],
-    ),
-  );
+          /*3*/
+          Icon(
+            Icons.star_border,
+            color: Colors.red[500],
+          ),
+          Text('41'),
+        ],
+      ),
+    );
+  }
 
   static void PhoneCall() {
     print("Phone call !");
@@ -93,104 +108,112 @@ class Announce {
 
   static Color color = Colors.blue;
 
-  static Widget buttonSection = Container(
-    margin: const EdgeInsets.only(top: 2),
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        _buildButtonColumn(color, Icons.call, 'CALL', PhoneCall),
-        _buildButtonColumn(color, Icons.link, 'LINK', GoToURL),
-        _buildButtonColumn(color, Icons.share, 'SHARE', Share),
-      ],
-    ),
-  );
-
-  static Widget textSection = Container(
-    padding: const EdgeInsets.all(10),
-    child: Text(
-      m_description,
-      softWrap: true,
-    ),
-  );
-
-  Widget announceSection = Center(
-    child: Container(
-      padding: EdgeInsets.all(10.0),
-      child: Column(
+  Widget CreateButtonSection() {
+    return new Container(
+      margin: const EdgeInsets.only(top: 2),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          titleSection,
-          Image.network(
-            m_imageURL,
-            width: 600,
-            height: 240,
-            fit: BoxFit.fitWidth,
-          ),
-          /*Image.asset(
-            'images/lake.jpg',
-            width: 600,
-            height: 240,
-            fit: BoxFit.cover,
-          ),*/
-          buttonSection,
-          textSection,
+          _buildButtonColumn(color, Icons.call, 'CALL', PhoneCall),
+          _buildButtonColumn(color, Icons.link, 'LINK', GoToURL),
+          _buildButtonColumn(color, Icons.share, 'SHARE', Share),
         ],
       ),
-    ),
-  );
+    );
+  }
+
+  Widget CreateTextSection() {
+    return new Container(
+      padding: const EdgeInsets.all(10),
+      child: Text(
+        '${data.description}',
+        softWrap: true,
+      ),
+    );
+  }
+
+  Widget CreateAnnounceSection() {
+    return new Center(
+      child: Container(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            CreateTitleSection(),
+            Image.network(
+              '${data.imageURL}',
+              width: 600,
+              height: 240,
+              fit: BoxFit.fitWidth,
+            ),
+            /*Image.asset(
+              'images/lake.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),*/
+            CreateButtonSection(),
+            CreateTextSection(),
+          ],
+        ),
+      ),
+    );
+  }
 
   //--------------------------------------------------------------------------
   // Light
   //--------------------------------------------------------------------------
-  static Widget titleSectionLight = Container(
-    padding: const EdgeInsets.all(4),
-    child: Row(
-      children: [
-        Expanded(
-          /*1*/
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              /*2*/
-              Text(
-                m_title,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
+  Widget CreateTitleSectionLight() {
+    return new Container(
+      padding: const EdgeInsets.all(4),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Text(
+                  '${data.title}',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              Row(
-                children: [
-                  Text(
-                    m_price.toString(),
-                    style: TextStyle(
-                      color: Colors.grey[500],
+                Row(
+                  children: [
+                    Text(
+                      '${data.price.toString()}',
+                      style: TextStyle(
+                        color: Colors.grey[500],
+                      ),
                     ),
-                  ),
-                  Text(' - '),
-                  Text(
-                    m_rate.toString(),
-                    style: TextStyle(
-                      color: Colors.grey[500],
+                    Text(' - '),
+                    Text(
+                      '${data.rate.toString()}',
+                      style: TextStyle(
+                        color: Colors.grey[500],
+                      ),
                     ),
-                  ),
-                  Text(
-                    '%',
-                    style: TextStyle(
-                      color: Colors.grey[500],
+                    Text(
+                      '%',
+                      style: TextStyle(
+                        color: Colors.grey[500],
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
-        /*3*/
-        Icon(
-          Icons.star_border,
-          color: Colors.red[500],
-        )
-      ],
-    ),
-  );
+          /*3*/
+          Icon(
+            Icons.star_border,
+            color: Colors.red[500],
+          )
+        ],
+      ),
+    );
+  }
 
   static Column _buildButtonColumnLight(
       Color color, IconData icon, Function() _f) {
@@ -220,25 +243,27 @@ class Announce {
     ),
   );
 
-  Widget announceSectionLight = Center(
-    child: Container(
-      padding: EdgeInsets.all(10.0),
-      child: Column(
-        children: [
-          titleSectionLight,
-          Row(
-            children: [
-              Image.network(
-                m_imageURL,
-                width: 310,
-                height: 122,
-                fit: BoxFit.fitWidth,
-              ),
-              buttonSectionLight,
-            ],
-          ),
-        ],
+  Widget CreateAnnounceSectionLight() {
+    return new Center(
+      child: Container(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            CreateTitleSectionLight(),
+            Row(
+              children: [
+                Image.network(
+                  '${data.imageURL}',
+                  width: 310,
+                  height: 122,
+                  fit: BoxFit.fitWidth,
+                ),
+                buttonSectionLight,
+              ],
+            ),
+          ],
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
--- Flutter/test_flutter/lib/test1.dart
+++ Flutter/test_flutter/lib/test1.dart
@@ -55,8 +55,18 @@ class _MyHomePageState extends State<MyHomePage> {
             FloatingActionButton(
               onPressed: () {
                 setState(() {
-                  Announce announce = new Announce();
-                  m_list.add(announce.announceSectionLight);
+                  AnnounceData data = new AnnounceData();
+                  data.title = 'Paris 18ème - Appartement 3 pièces';
+                  data.price = 437000;
+                  data.rate = 10.7;
+                  data.description =
+                      'Paris 18ème - Abbesses - Dans une charmante copropriété Montmartroise calme et verdoyante, idéalement située proche de la rue des Abbesses et de la rue Lepic, un appartement composé d une grande pièce principale, avec une cuisine aménagée et un coin bureau, une salle d eau et un w.-c. séparé. Au sous-sol, une cave. Honoraires d agence à la charge de l acquéreur inclus :  4,61 % soit 20 000 € d honoraires. Prix hors honoraires : 434 000 €.';
+                  data.announceURL =
+                      'https://www.laforet.com/agence-immobiliere/paris18abbesses/acheter/paris-18/appartement-1-piece-19493910';
+                  data.imageURL =
+                      'https://laforetbusiness.laforet-intranet.com/office9/laforet_gaillon/catalog/images/pr_p/1/9/5/0/9/9/3/2/19509932a.jpg';
+                  Announce announce = new Announce(data);
+                  m_list.add(announce.CreateAnnounceSectionLight());
                   print(">>>>>>>>>>>>>>>>>>>>>>>>>   ADD Button pressed");
                 });
               },
